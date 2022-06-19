fc423c0f8a12748e83e546522b60dc2306fe3332
BernardoModerno <bernardo.moderno@gmail.com>
2022-06-19 20:35:43 -0300

    Tela Principal (Parte 1)

--- mobiledelivery/lib/src/pages/login/login_page.dart
+++ mobiledelivery/lib/src/pages/login/login_page.dart
@@ -6,8 +6,78 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('LOGIN PAGE'),
+      bottomNavigationBar: Container(
+        height: 50,
+        child: _textDontHaveAccount(),
+      ),
+      body: Stack( //Posicionar elementos encima do outro
+        children: [
+          _backgroundCover(context),
+          Column( //Posicionar elementos um debaixo do outro (Vertical)
+            children: [
+              _imageCover(),
+              _textAppName()
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _backgroundCover(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.4,
+      color: Colors.amber,
+    );
+  }
+
+  Widget _textAppName() {
+    return Text(
+      'DELIVERY APP',
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Colors.black
+      ),
+    );
+  }
+
+  Widget _textDontHaveAccount() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Não tem conta?',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 17
+          ),
+        ),
+        SizedBox(width: 7),
+        Text(
+          'Registre-se aqui',
+          style: TextStyle(
+            color: Colors.amber,
+            fontWeight: FontWeight.bold,
+            fontSize: 17
+          ),
+        ),
+      ],
+    );
+  }
+
+  // Private
+  Widget _imageCover() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top: 20, bottom: 15),
+        alignment: Alignment.center,
+        child: Image.asset(
+          'assets/img/delivery.png',
+          width: 130,
+          height: 130,
+        ),
       ),
     );
   }
