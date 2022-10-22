750812627a65c110d14fd2bd951192b059c36948
BernardoModerno <bernardo.moderno@gmail.com>
2022-10-22 20:13:22 -0300

    Tela Principal (Parte 2)

--- mobiledelivery/lib/main.dart
+++ mobiledelivery/lib/main.dart
@@ -29,6 +29,23 @@ class _MyAppState extends State<MyApp> {
       getPages: [
         GetPage(name: '/', page: () => LoginPage())
       ],
+      theme: ThemeData(
+        primaryColor: Colors.amber,
+        colorScheme: ColorScheme(
+          secondary: Colors.amber,
+          primary: Colors.amberAccent,
+          brightness: Brightness.light, 
+          onBackground: Colors.grey,
+          onPrimary: Colors.grey,
+          surface: Colors.grey,
+          onSurface: Colors.grey,
+          error: Colors.grey,
+          onError: Colors.grey,
+          onSecondary: Colors.grey,
+          background: Colors.grey,
+
+        )
+      ),
       navigatorKey: Get.key,
     );
   }
--- mobiledelivery/lib/src/pages/login/login_page.dart
+++ mobiledelivery/lib/src/pages/login/login_page.dart
@@ -13,6 +13,7 @@ class LoginPage extends StatelessWidget {
       body: Stack( //Posicionar elementos encima do outro
         children: [
           _backgroundCover(context),
+          _boxForm(context),
           Column( //Posicionar elementos um debaixo do outro (Vertical)
             children: [
               _imageCover(),
@@ -27,7 +28,7 @@ class LoginPage extends StatelessWidget {
   Widget _backgroundCover(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: MediaQuery.of(context).size.height * 0.4,
+      height: MediaQuery.of(context).size.height * 0.42,
       color: Colors.amber,
     );
   }
@@ -39,6 +40,91 @@ class LoginPage extends StatelessWidget {
         fontSize: 20,
         fontWeight: FontWeight.bold,
         color: Colors.black
+      ),
+    );
+  }
+
+  Widget _boxForm(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.45,
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.35, left: 50, right: 50),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black54,
+            blurRadius: 15,
+            offset: Offset(0, 0.75)
+          )
+        ]
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            _textYourInfo(),
+            _textFieldEmail(),
+            _textFieldPassword(),
+            _buttonLogin()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldEmail() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Email',
+          prefixIcon: Icon(Icons.email)
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      child: TextField(
+        keyboardType: TextInputType.text,
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Senha',
+          prefixIcon: Icon(Icons.lock)
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonLogin() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+      child: ElevatedButton(
+        onPressed: () {} ,
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(vertical: 15)
+        ),
+        child: Text(
+          'Login',
+          style: TextStyle(
+            color: Colors.black
+          ),
+        )
+      ),
+    );
+  }
+
+  Widget _textYourInfo() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, bottom: 45),
+      child: Text(
+        'Entre com essas informações',
+        style: TextStyle(
+          color: Colors.black,
+        ),
       ),
     );
   }
