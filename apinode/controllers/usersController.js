ee07e214624c771a7729ee5e3ae33f0042d1eb0b
BernardoModerno <bernardo.moderno@gmail.com>
2022-10-24 22:17:34 -0300

    JWT Node

--- apinode/controllers/usersController.js
+++ apinode/controllers/usersController.js
@@ -1,6 +1,67 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
 
 module.exports = {
+
+    login(req, res) {
+
+        const email = req.body.email;
+        const password = req.body.password;
+
+        User.findByEmail(email, async (err, myUser) => {
+            
+            console.log('Error ', err);
+            console.log('USUÁRIO ', myUser);
+
+            if (err) {
+                return res.status(501).json({
+                    success: false,
+                    message: 'Houve um error com o registro do usuário',
+                    error: err
+                });
+            }
+
+            if (!myUser) {
+                return res.status(401).json({ // O cliente não tem autorização para realizar essa petição (401)
+                    success: false,
+                    message: 'O email não foi encontrado'
+                });
+            }
+
+            const isPasswordValid = await bcrypt.compare(password, myUser.password);
+
+            if (isPasswordValid) {
+                const token = jwt.sign({id: myUser.id, email: myUser.email}, keys.secretOrKey, {});
+
+                const data = {
+                    id: myUser.id,
+                    name: myUser.name,
+                    lastname: myUser.lastname,
+                    email: myUser.email,
+                    phone: myUser.phone,
+                    image: myUser.image,
+                    session_token: `JWT ${token}`
+                }
+
+                return res.status(201).json({
+                    success: true,
+                    message: 'O usuário foi autenticado',
+                    data: data // O ID do novo usuário que se registrou
+                });
+
+            }
+            else {
+                return res.status(401).json({ // O cliente não tem autorização para realizar essa petição (401)
+                    success: false,
+                    message: 'A senha está incorreta'
+                });
+            }
+
+        });
+
+    },
 
     register(req, res) {
 
--- apinode/routes/userRoutes.js
+++ apinode/routes/userRoutes.js
@@ -8,5 +8,6 @@ module.exports = (app) => {
     // DELETE -> excluir dados
 
     app.post('/api/users/create', usersController.register);
+    app.post('/api/users/login', usersController.login);
 
 }
